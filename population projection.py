00dd75569c4541f908a984e2ffe1c5910fcf6fe1
Jon Mockbee <mockbee1000@gmail.com>
2022-02-26 16:15:48 -0500

    moving to annual percentage

---
+++ population projection.py
@@ -0,0 +1,9 @@
+city_name = "Istanbul,Turkey"
+pop_1927 = 691000
+pop_2017 = 15029231
+
+pop_change = pop_1927 - pop_2017
+
+percentage_gr = (pop_change / pop_1927) * 100
+
+annual_gr = percentage_gr/(2017-1927)
